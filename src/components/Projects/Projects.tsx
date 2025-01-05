dbc9904f85e6db305630b44c3bb99721085b4f8c
Brandon Mason <bjm241@txstate.edu>
2025-01-05 15:46:19 -0600

    Nearly completed individual project component.

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,21 +2,38 @@ import { MantineComponent, Container, Title } from '@mantine/core';
 import classes from './Projects.module.css';
 import { forwardRef } from 'react';
 import SingleProj from '../SingleProj/SingleProj';
+import projects from '../../../public/projects.json';
 
 interface ProjectProps {
     headerHeight: number;
 }
 
+interface Project {
+    name: string;
+    description: string;
+    image: string;
+    github: string;
+    deployed: string;
+}
+
 const Projects = forwardRef<MantineComponent<any>, ProjectProps>((props, projectRef) => {
+
+    const projectList = projects.map((project: Project) => {
+        return <SingleProj
+            name={project.name}
+            description={project.description}
+            image={project.image}
+            github={project.github}
+            deployed={project.deployed}
+        />
+    });
+
     return (
         <Container mih={'100vh'} ref={projectRef} pt={`calc(${props.headerHeight}px + 1vh`} px={0} ml={0} className={classes.container}>
             <Title order={2} className={classes.title}>Projects</Title>
-            <SingleProj 
-                name="Recordable Midi Piano"
-                description="This is a description of the project."
-                imgSrc="project-images/piano.png"
-                hostLink="https://example.com"
-                repoLink="https://github.com/example/repo"/>
+            <Container pos={'relative'} w={'100vw'} p={0} className={classes.projectsRoot} >
+                {projectList}
+            </Container>
         </Container>
     );
 });
--- src/components/SingleProj/SingleProj.tsx
+++ src/components/SingleProj/SingleProj.tsx
@@ -1,29 +1,44 @@
 import React from 'react';
-import { Container, Group, Image, Stack, Text, Title } from '@mantine/core';
+import { Container, Divider, Flex, Group, Image, Space, Stack, Text, Title } from '@mantine/core';
 import classes from './SingleProj.module.css';
+import { SiGithub } from "react-icons/si";
+import { LuExternalLink } from "react-icons/lu";
 
 interface SingleProjProps { 
     name: string;
     description: string;
-    imgSrc: string;
-    hostLink: string; 
-    repoLink: string;
+    image: string;
+    github: string;
+    deployed: string; 
 }
 
 const SingleProj: React.FC<SingleProjProps> = (props) => {
     return (
-        <Stack>
-            <Title order={3}>{props.name}</Title>
-            <Group>
-                <Image src={props.imgSrc} alt={props.name.toLowerCase().replace(/\s/g, '-')} 
-                    style={{ borderRadius: '10px' }} 
-                    classNames={{
-                        root: classes.imgRoot,
-                    }}
-                />
-                <Text>{props.description}</Text>
-            </Group>
-        </Stack>
+        <>
+            <Divider mb={'2rem'} size={1.5} labelPosition='left' color={'var(--mantine-color-zomp)'} label={<Title order={3} className={classes.title}>{props.name}</Title>} />
+            <Stack pb={'2rem'} pl={'1.5rem'}>
+                <Flex wrap='nowrap' dir='column'>
+                    <Image src={props.image} alt={props.name.toLowerCase().replace(/\s/g, '-')} 
+                        style={{ borderRadius: '10px' }} 
+                        classNames={{
+                            root: classes.imgRoot,
+                        }}
+                    />
+                    <Stack ml={'1.5rem'}>
+                        <Text style={{ verticalAlign: 'top', alignSelf: 'flex-start', textWrap: 'wrap' }}>{props.description}</Text>
+                        <Group>
+                            <a href={props.github} target="_blank" rel="noreferrer">
+                                <SiGithub size={35} color='var(--mantine-color-viridian)' />
+                            </a>
+                            <Space />
+                            <a href={props.deployed} target="_blank" rel="noreferrer">
+                                <LuExternalLink size={35} color='var(--mantine-color-viridian)' />
+                            </a>
+                        </Group>
+                    </Stack>
+                </Flex>
+            </Stack>
+        </>
     );
 };
 
